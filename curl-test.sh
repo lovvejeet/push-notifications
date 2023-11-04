9a5558e73fd2a9ba13ff965fddcb3f80d8d0d554
lovvejeet <135307361+lovvejeet@users.noreply.github.com>
2023-11-04 13:07:25 +0530

    Update curl-test.sh

--- curl-test.sh
+++ curl-test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SERVER_KEY='<your-server-key-from-cloud-messaging-config>'
-DEVICE_REG_TOKEN='<device-token-you-get-on-webpage>'
+SERVER_KEY='AAAASMZ2ULc:APA91bELhOBAM8WsBcNBlPNNG1pWWOl-db7A4j7uv3HjDcx_axUIgxyVyWW9NehhiBW_rjaipGVZXmaa2rhYgiYc5tv2yE575qxuAtbgg7z1iMgnnOri4MsBFArZihltcgSvgiSV-t40'
+DEVICE_REG_TOKEN='cD1eQe2kBVNwI7x6HuwuVY:APA91bGAFi6p-N_TVvbNRzK89OQbGzgUXABatJxoL5khqXJvHR9-dnEJvIwNZFcXrJP2awuF3w0qbDr4g_KaPkOWAWSdPB-qnXUJEVN6mdGLu37fwPEHdgW4oeym45_0wTRf-NJYIHbR'
 
 CMD=$(cat <<END
 curl -X POST -H "Authorization: key=$SERVER_KEY" -H "Content-Type: application/json"
